2a10d8e3e3223c6363b4a837d09b9025309e0169
Fides Linga <fides.linga@mail.utoronto.ca>
2019-04-02 10:26:27 -0400

    SN-2: Changing theme to black and white, adding box shadow to module cards.

--- src/components/containers/ModuleLanding.js
+++ src/components/containers/ModuleLanding.js
@@ -4,11 +4,13 @@ import ModuleCardList from './ModuleCardList';
 class ModuleLanding extends React.Component {
   render() {
     return (
-      <div className="landing top container-fluid">
-        <div className="col-12">
-         <h2 className="text-center">sparknodes</h2>
+      <div className="page light">
+        <div className="landing top container-fluid">
+          <div className="col-12">
+           <h2 className="text-center">sparknodes</h2>
+          </div>
+          <ModuleCardList/>
         </div>
-        <ModuleCardList/>
       </div>
     );
   }
