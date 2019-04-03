6601b5677dd2531118cbd22a9408a95362d9a2c3
Fides Linga <fides.linga@mail.utoronto.ca>
2019-04-03 15:32:39 -0400

    SN-3: Attempting to link module section list toggler in module page component.

--- src/components/containers/ModuleSectionList.js
+++ src/components/containers/ModuleSectionList.js
@@ -1,22 +1,28 @@
 import React from 'react';
-import FullDialog from '../../ui/FullDialog';
+import FullDialog from '../ui/FullDialog';
 import { Link } from 'react-router-dom';
 
 class ModuleSectionList extends React.Component {
   render() {
-    return (
-      <FullDialog>
-        <Link to='/'>
-          Introduction
-        </Link>
-        <Link to='/'>
-          Nodes
-        </Link>
-        <Link to='/'>
-          Circular Linked List
-        </Link>
-      </FullDialog>
-    );
+    if (this.props.show) {
+      return (
+        <FullDialog>
+          <Link to='/'>
+            Introduction
+          </Link>
+          <Link to='/'>
+            Nodes
+          </Link>
+          <Link to='/'>
+            Circular Linked List
+          </Link>
+        </FullDialog>
+      );
+    } else {
+      return (
+        <div></div>
+      )
+    }
   }
 }
 
--- src/components/containers/module-page/ModulePage.js
+++ src/components/containers/module-page/ModulePage.js
@@ -1,21 +1,34 @@
 import React from 'react';
+
 import ModuleRightPage from './ModuleRightPage';
 import ModuleLeftPage from './ModuleLeftPage';
+import ModuleSectionList from '../ModuleSectionList';
 
 import LinkedListIntroLeft from '../../modules/linked-lists/sections/LinkedListIntroLeft';
 import LinkedListIntroRight from '../../modules/linked-lists/sections/LinkedListIntroRight';
 import LinkedListNodeLeft from '../../modules/linked-lists/sections/LinkedListNodeLeft';
 
 class ModulePage extends React.Component {
+  state = {
+    showModuleList: false
+  };
+
+  moduleListButtonHandler = () => {
+    this.setState({
+      showModuleList: !this.state.showModuleList
+    });
+  }
+
   render() {
     document.querySelector('body').className = 'theme-dark'
     return (
       <div className="module-page container-fluid">
-        <Button className="btn module-list-btn">
+        <button className="btn module-list-btn" onClick={this.moduleListButtonHandler}>
           <i class="material-icons">
           more_vert
           </i>
-        </Button>
+        </button>
+        <ModuleSectionList show={this.state.showModuleList}/>
         <div className="row">
           <ModuleLeftPage>
             <LinkedListIntroLeft/>
