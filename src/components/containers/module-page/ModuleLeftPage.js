06fb04685af4825fa4458f8021fdafae3e223160
Fides Linga <fides.linga@mail.utoronto.ca>
2019-04-03 13:06:41 -0400

    SN-3: Changed some padding in module pages.

--- src/components/containers/module-page/ModuleLeftPage.js
+++ src/components/containers/module-page/ModuleLeftPage.js
@@ -5,6 +5,7 @@ class ModuleLeftPage extends React.Component {
     return (
       <div className="left col-12 col-md-5 col-lg-4">
         {this.props.children}
+        <hr/>
       </div>
     );
   }
